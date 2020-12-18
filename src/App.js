2829178f10ad9febb78617dd49c90292eee1ff07
EX1STENCE <prasadnitin05@gmail.com>
2020-12-18 20:52:50 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+	const [Count, setCount] = useState(() => 0);
+	return (
+		<div>
+			<div className='container card'>
+				<div className='card-title'>Counter App</div>
+				<div className='row'>
+					<div className='content'>
+						<button onClick={() => setCount((prevCount) => prevCount - 1)}>
+							-
+						</button>
+						<span>{Count}</span>
+						<button onClick={() => setCount((prevCount) => prevCount + 1)}>
+							+
+						</button>
+					</div>
+					<div className='reset'>
+						<button onClick={() => setCount((prevCount) => (prevCount = 0))}>
+							Reset
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
